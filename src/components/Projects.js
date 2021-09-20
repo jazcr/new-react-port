3137d51b60d34419b99f2cd467334d93d14b9837
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-19 22:24:52 -0600

    project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,1 +1,53 @@
-.
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+/********** TEMPLATE FOR PROJECT CARDS **********/
+const useStyles = makeStyles((theme) => ({
+    justify: {
+        display: 'flex',
+        justtifyContent: 'center',
+    },
+    card: {
+        margin: theme.spacing(2),
+    },
+}));
+
+export const MediaCard = ({ projects }) => {
+    const classes = useStyles();
+
+    return (
+        <Grid class="min-vh-100">
+            <Grid className={classes.justify}>
+                {projects.map((project) => (
+                    <Card className={classes.card} sx={{ maxWidth: 333 }} >
+                        <CardMedia
+                            component="img"
+                            image={project.image}
+                            alt={project.name}
+                        />
+                        <CardContent >
+                            <Typography gutterBottom variant="h5" component="div">
+                                {`${project.name}`}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {`${project.info}`}
+                            </Typography>
+                        </CardContent>
+                        <CardActions class="d-flex justify-content-center p-2">
+                            <a id="icons" class="align-self-center p-2" href={project.repo} aria-label="Github" target="_blank"><i class="fab fa-github fa-3x socialicons"></i></a>
+                            <a id="icons" class="align-self-center p-2" href={project.deployed} aria-label="Deployed App" target="_blank">Deployed App</a>
+                        </CardActions>
+                    </Card>
+                ))}
+            </Grid>
+        </Grid>
+    );
+};
+
+export default MediaCard;
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,1 +1,36 @@
-.
+const projectData = [
+    {
+        name: '4thKind',
+        image: './images/comp.jpg',
+        info: 'A simple blog site that allows extraterrestrial believers to create posts and drop pins where they hvae seen suspicious activity. Created in a team setting.',
+        tech: 'JavaScript, Handlebars, CSS',
+        repo: 'https://github.com/jazcr/4thKind',
+        deployed: 'https://the-4th-kind.herokuapp.com',
+    },
+    {
+        name: 'Make My Day App',
+        image: './images/comp.jpg',
+        info: 'Simply generate random GIFs, fox images, and inspirational quotes to brighten your day. Created in a team setting.',
+        tech: 'JavaScript/jQuery, HTML5, CSS',
+        repo: 'https://github.com/jazcr/Make_My_Day_app',
+        deployed: 'https://jazcr.github.io/Make_My_Day_app/index.html',
+    },
+    {
+        name: 'Work-Day Planner',
+        image: './images/comp.jpg',
+        info: 'This is a simple scheduling app that allows you to input events for each hour of the typical work day. These events are saved to local storage, so they will not be lost when the page is refreshed.',
+        tech: 'JavaScript, HTML5, CSS',
+        repo: 'https://github.com/jazcr',
+        deployed: 'https://jazcr.github.io/Work-Day-Schedule/',
+    },
+    {
+        name: 'Weather App',
+        image: './images/comp.jpg',
+        info: 'This simple weather app where you enter a city name into the search box and the current weather andthe future 5 day forecast displays.',
+        tech: 'JavaScript, HTML5, CSS3',
+        repo: 'https://github.com/jazcr/weather-app',
+        deployed: 'https://jazcr.github.io/weather-app',
+    }
+];
+
+export default projectData;
