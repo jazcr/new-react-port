64b7f5e99e6607432d07837e66fbd8818f971db6
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-19 22:04:19 -0600

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,60 @@
-.
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import headShot from '../Assets/picture.jpg';
+
+const useStyles = makeStyles((theme) => ({
+    font: {
+        fontFamily: 'Nunito, Georgia, cursive, sans-serif',
+        color: 'whitesmoke',
+        padding: '5px',
+        margin: '1%',
+        lineHeight: '22px',
+        fontSize: '1.20rem',
+    },
+}));
+
+export default function FullWidthGrid() {
+    const classes = useStyles();
+    return (
+        <Box class="min-vh-100" sx={{ flexGrow: 1 }}>
+            <Grid class="d-flex justify-content-center">
+                <img src={headShot} alt="Resume" />
+            </Grid>
+            <Grid class="d-flex justify-content-center" container spacing={2}>
+                <Container id="about-me">
+                    <h1 class="m-1">
+                        About Me
+                    </h1>
+                    <Typography className={classes.font} margin={2} variant="body1" gutterBottom>
+                    Belive it or not, my path to Web Development started in <em>Psychology</em>. I started my college journey learning about the human mind and behavior because I knew I wanted to positively impact people's lives. Little did I know, building convenient, yet beautiful websites has become one of the easiest ways to make an impact on someone's daily life.
+                    </Typography>
+                </Container>
+            </Grid>
+            <Grid class="d-flex justify-content-center" container spacing={2}>
+                <Container id="about-me">
+                    <h1 class="m-1">
+                        Skills
+                    </h1>
+                    <Typography className={classes.font} margin={2} variant="body1" gutterBottom>
+                        CLI, Git, HTML5, CSS, JavaScript/jQuery, MERN - (Mongoose, Express.js, React.js, Node.js), MySQL
+                    </Typography>
+                </Container>
+            </Grid>
+            <Grid class="d-flex justify-content-center" container spacing={2}>
+                <Container id="about-me">
+                    <h1 class="m-1">
+                        Education
+                    </h1>
+                    <Typography className={classes.font} margin={2} variant="body1" gutterBottom>
+                        Fullstack WebDevelopment Bootcamp Certificate - Universit of Denver 2021
+                        Associates of Arts - Front Range Community College 2018
+                    </Typography>
+                </Container>
+            </Grid>
+        </Box>
+    );
+}
